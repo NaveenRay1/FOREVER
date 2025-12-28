ba386de1b68de263a0c60e1c40dc7288bd1a6a9a
Naveen Kumar Ray <218852547+NaveenRay1@users.noreply.github.com>
2025-12-29 01:26:12 +0530

    completed hero section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import LatestColletion from '../components/LatestColletion'
 
 const Home = () => {
   return (
     <div>
-      
+      <Hero/>
+      <LatestColletion/>
     </div>
   )
 }
