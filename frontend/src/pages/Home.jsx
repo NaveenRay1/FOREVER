ebd58241997413fd5633539505d357cf94944a93
Naveen Kumar Ray <218852547+NaveenRay1@users.noreply.github.com>
2025-12-29 02:22:31 +0530

    added OurPolicy & NewsletterBox

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestColletion from '../components/LatestColletion'
 import BestSeller from '../components/BestSeller'
-
+import OurPolicy from '../components/OurPolicy'
+import NewsletterBox from '../components/NewsletterBox'
 const Home = () => {
   return (
     <div>
       <Hero/>
       <LatestColletion/>
       <BestSeller/>
+      <OurPolicy/>
+      <NewsletterBox/>
     </div>
   )
 }
