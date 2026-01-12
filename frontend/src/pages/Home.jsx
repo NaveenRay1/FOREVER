cb20d750160599f59a6df8b508130a8531356f7b
Naveen Kumar Ray <218852547+NaveenRay1@users.noreply.github.com>
2026-01-13 00:13:11 +0530

    Fix NewsLetterBox/Home imports and build

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import LatestColletion from '../components/LatestColletion'
-import BestSeller from '../components/BestSeller'
-import OurPolicy from '../components/OurPolicy'
-import NewsletterBox from '../components/NewsLetterBox'
+import React from 'react';
+import Hero from '../components/Hero';
+import LatestColletion from '../components/LatestColletion';
+import BestSeller from '../components/BestSeller';
+import OurPolicy from '../components/OurPolicy';
+import NewsLetterBox from '../components/NewsLetterBox'; // match the filename exactly
+
 const Home = () => {
   return (
     <div>
-      <Hero/>
-      <LatestColletion/>
-      <BestSeller/>
-      <OurPolicy/>
-      <NewsletterBox/>
+      <Hero />
+      <LatestColletion />
+      <BestSeller />
+      <OurPolicy />
+      <NewsLetterBox />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
